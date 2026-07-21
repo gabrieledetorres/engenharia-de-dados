8956c38f5b89a656049741aee58ed2aaefd31849
Gabriele Torres <gabriele.torres@delmiro.ufal.br>
2026-07-21 11:27:23 -0300

    Conclui revisão de variáveis, funções e métodos

---
+++ 01 -python/python-fundamentos/main.py
@@ -0,0 +1,28 @@
+#Exercício 1
+nome_empresa = "Tech Solutions"
+quantidade_clientes = 1542
+
+print(nome_empresa)
+print("nome_empresa")
+
+print(quantidade_clientes)
+print("quantidade_clientes")
+
+print(nome_empresa.upper())
+print(nome_empresa.lower())
+
+print(type(nome_empresa))
+print(type(quantidade_clientes))
+
+#Exercício 2
+
+nome = "Ana"
+idade = 29
+cargo = "Engenheira de Dados"
+
+print(nome)
+print(idade)
+print(cargo)
+
+empresa = 'Snake Data'
+print(type(empresa.upper()))
